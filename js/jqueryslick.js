2533225b472666cda18849d05576544285852a10
Rootytootyfrooty <root.kathryn@gmail.com>
2026-04-27 12:39:56 +0100

    Added coding examples
    Also tried to jquery slick my examples page but it failed and I need to look into it

---
+++ js/jqueryslick.js
@@ -0,0 +1,11 @@
+// $(document).ready(function () {
+//     $('.example-carousel').slick({
+//         autoplay: false,
+//         arrows: false,
+//         adaptiveHeight: true,
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         variableWidth: false,
+//         infinite: true
+//     });
+// });
